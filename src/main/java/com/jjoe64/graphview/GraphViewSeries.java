db241bd2f16104f8d47bba1b69ee86552ebbb4f3
Nejc Tomsic <nejc@modrajagoda.si>
2014-07-02 19:48:05 +0200

    Add drawDataPoints field to GraphViewSeriesStyle class
    This will allow for per GraphViewSeries setting of drawing data points.

--- src/main/java/com/jjoe64/graphview/GraphViewSeries.java
+++ src/main/java/com/jjoe64/graphview/GraphViewSeries.java
@@ -33,6 +33,7 @@ public class GraphViewSeries {
 	static public class GraphViewSeriesStyle {
 		public int color = 0xff0077cc;
 		public int thickness = 3;
+        public boolean drawDataPoints = false;
 		private ValueDependentColor valueDependentColor;
 
 		public GraphViewSeriesStyle() {
